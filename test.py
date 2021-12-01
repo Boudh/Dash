4f2292462aa198a00931d89663478c1677f55de0
Rphaël Giraudot <raphael.giraudot@gmail.com>
2021-12-01 17:49:18 +0100

    fichier au propre

--- test.py
+++ test.py
@@ -1,24 +1,30 @@
+#manipulation des données
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import os
-import plotly.figure_factory as ff
-import plotly.express as px
+import numpy as np
 
-
+#requêtes à l'API
 import requests
 
-import numpy as np
+#plots
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+import plotly.express as px
 import seaborn as sns
+
+#récupérer modèles & explainer
 import joblib
 
+#interprétabilité
+import shap
+shap.initjs()
+
+#dashboard
 import streamlit as st
 import streamlit.components.v1 as components
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-import shap
-shap.initjs()
-
+#définition de fonctions
 #fonction requête API
 def get_data(url):
     resp = requests.get(url)
@@ -32,34 +38,20 @@ def distplots(data,var,height=600):
     plot = ff.create_distplot([x1,x2], [0,1], show_hist=False, colors=['green','red'])
     plot.add_vline(x,line_width=2,line_dash="dash",line_color="orange",annotation_text="Client",annotation_font_color='orange',annotation_font_size=18)
     plot.update_layout(height=height)
-    titre = "Distribution de la variable: "+var+"\n& Positionnement du client"
+    titre = "Distribution de la variable: "+var+" & Positionnement du client"
     plot.update_layout(title_text=titre)
     return plot
 
-#fonction shap plots
-#def st_shap(plot, height=None):
-#    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-#    components.html(shap_html, height=height)
-
-#test_path = os.path.dirname(os.path.abspath(__file__))
-#st.write(test_path)
-#files = os.listdir(test_path)
-#for f in files:
-#    st.write(f)
-#st.write(test_path+'\modele.sav')
-
+#chargement des données
 #chargement du modèle
 model = joblib.load('modele.sav')
-
 #chargement de l'explainer SHAP
 explainer = joblib.load('explainer.sav')
-
 #chargement des fichiers de travail
 #chargement de l'échantillon
 clients = pd.read_csv('sample.csv')
 #colonne SK_ID_CURR en index
 clients.set_index('SK_ID_CURR', inplace = True)
-
 #chargement des résultats de la prédiction (pour les graphs)
 clients_pred = pd.read_csv('sample_pred.csv')
 #colonne SK_ID_CURR en index
@@ -141,8 +133,6 @@ with st.expander("Global Feature Importance"):
     st.write('EXT_SOURCE_2 : des valeurs élevées de cette variable, a droite de l\'axe vertical, vont impacter négativement la valeur de sortie')
     st.write('bureau_DAYS_CREDIT_sum : à l\'inverse de EXT_SOURCE_2, des valeurs élevées de cette variable vont impacter positivement la veleur de la sortie')
 
-
-
 #On récupère le 10 features les plus importantes 
 feature_names = shap_values.feature_names
 shap_df = pd.DataFrame(shap_values.values, columns=feature_names)
@@ -151,13 +141,6 @@ shap_importance = pd.DataFrame(list(zip(feature_names, vals)), columns=['col_nam
 shap_importance.sort_values(by=['feature_importance_vals'], ascending=False, inplace=True)
 top_ten = shap_importance['col_name'].head(10).reset_index(drop=True)
 top_ten = pd.DataFrame(top_ten)
-
-#fonctio qui 
-#def feats_plot(id_client, data, var):
-#    x=data.loc[id_client][var]
-    #axarr = f.add_subplot(1,1,1)
- #   sns.kdeplot(data.loc[data['PRED'] == 0, var], label = 'TARGET == 0')
-  #  sns.kdeplot(data.loc[data['PRED'] == 1, var], label = 'TARGET == 1').axvline(x)
 
 #Plus d'informations
 with st.expander("Plus de détail"):
@@ -174,59 +157,7 @@ with st.expander("Plus de détail"):
     st.plotly_chart(distplots(clients_pred,var_2), use_container_width=True)
 
     st.write("Analyse bivariée :")
-    titre="Croisement des variables :\n"+var_1+" & "+var_2
+    titre="Croisement des variables : "+var_1+" & "+var_2
     scat_plot = px.scatter(clients_pred, x=var_1, y=var_2, color="SCORE",
     title=titre, color_continuous_scale='rdylgn_r')
     st.plotly_chart(scat_plot, use_container_width=True)
-
-
-
-#st.write(var_1, ' Séléctionnée !')
-#st.write(clients_pred.loc[id_client][var_1])
-
-#st.pyplot(feats_plot(id_client,clients_pred,var_1))
-
-#st.write('Plotly :')
-#x1 = clients_pred.loc[clients_pred['PRED'] == 0, var_1]
-#x2 = clients_pred.loc[clients_pred['PRED'] == 1, var_1]
-#x=clients_pred.loc[id_client][var_1]
-
-
-
-#plot_v1 = ff.create_distplot([x1,x2], [0,1], show_hist=False, colors=['green','red']).add_vline(x,line_width=2,line_dash="dash",line_color="orange",annotation_text="Client",annotation_font_color='orange',annotation_font_size=18)
-#plot_v1.update_layout(height=600)
-#st.plotly_chart(plot_v1, use_container_width=True)
-
-#st.write("avec fonction")
-
-
-
-
-#st.write(var_2, ' Séléctionnée !')
-
-#st.pyplot(feats_plot(id_client,clients_pred,var_2))
-
-#x1 = clients_pred.loc[clients_pred['PRED'] == 0, var_2]
-#x2 = clients_pred.loc[clients_pred['PRED'] == 1, var_2]
-#x=clients_pred.loc[id_client][var_2]
-
-#plot_v2 = ff.create_distplot([x1,x2], [0,1], show_hist=False, colors=['green','red']).add_vline(x,line_width=2,line_dash="dash",annotation_text="Client",annotation_font_size=18)
-#plot_v2.update_layout(height=600)
-#st.plotly_chart(plot_v2, use_container_width=True)
-
-#bivarié
-#color_map = plt.cm.get_cmap('RdYlGn')
-#col_map = color_map.reversed()
-
-#def biplot(v1,v2,data,colmap,hue):
- #   plt.scatter(data[v1],data[v2],c=data[hue],cmap=colmap)
-    #plt.colorbar
-
-#st.pyplot(biplot(var_1,var_2,clients_pred,col_map,'SCORE'))
-
-
-
-#st.write(top_ten)
-
-
-#st_shap(waterfall)
