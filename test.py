313ae427673736d7a0a128c6043f84ee3d723f6f
Rphaël Giraudot <raphael.giraudot@gmail.com>
2021-12-01 16:19:27 +0100

    test couleur orange graphs

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import os
+import plotly.figure_factory as ff
 
 import requests
 
@@ -22,9 +23,9 @@ def get_data(url):
     return resp.json()
 
 #fonction shap plots
-def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-    components.html(shap_html, height=height)
+#def st_shap(plot, height=None):
+#    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
+#    components.html(shap_html, height=height)
 
 #test_path = os.path.dirname(os.path.abspath(__file__))
 #st.write(test_path)
@@ -126,25 +127,25 @@ with st.expander("Global Feature Importance"):
     st.write('EXT_SOURCE_2 : des valeurs élevées de cette variable, a droite de l\'axe vertical, vont impacter négativement la valeur de sortie')
     st.write('bureau_DAYS_CREDIT_sum : à l\'inverse de EXT_SOURCE_2, des valeurs élevées de cette variable vont impacter positivement la veleur de la sortie')
 
-#Variables
 
-#top10 features
+
+#On récupère le 10 features les plus importantes 
 feature_names = shap_values.feature_names
 shap_df = pd.DataFrame(shap_values.values, columns=feature_names)
 vals = np.abs(shap_df.values).mean(0)
 shap_importance = pd.DataFrame(list(zip(feature_names, vals)), columns=['col_name', 'feature_importance_vals'])
 shap_importance.sort_values(by=['feature_importance_vals'], ascending=False, inplace=True)
-
 top_ten = shap_importance['col_name'].head(10).reset_index(drop=True)
 top_ten = pd.DataFrame(top_ten)
 
-#plots features
-def feats_plot(id_client, data, var):
-    x=data.loc[id_client][var]
+#fonctio qui 
+#def feats_plot(id_client, data, var):
+#    x=data.loc[id_client][var]
     #axarr = f.add_subplot(1,1,1)
-    sns.kdeplot(data.loc[data['PRED'] == 0, var], label = 'TARGET == 0')
-    sns.kdeplot(data.loc[data['PRED'] == 1, var], label = 'TARGET == 1').axvline(x)
+ #   sns.kdeplot(data.loc[data['PRED'] == 0, var], label = 'TARGET == 0')
+  #  sns.kdeplot(data.loc[data['PRED'] == 1, var], label = 'TARGET == 1').axvline(x)
 
+#liste pour séléctionner la 1ere feature 
 var_1 = st.selectbox('Please select a feature :',top_ten)
 #st.write(var_1, ' Séléctionnée !')
 #st.write(clients_pred.loc[id_client][var_1])
@@ -152,12 +153,13 @@ var_1 = st.selectbox('Please select a feature :',top_ten)
 #st.pyplot(feats_plot(id_client,clients_pred,var_1))
 
 #st.write('Plotly :')
-import plotly.figure_factory as ff
 x1 = clients_pred.loc[clients_pred['PRED'] == 0, var_1]
 x2 = clients_pred.loc[clients_pred['PRED'] == 1, var_1]
 x=clients_pred.loc[id_client][var_1]
 
-plot_v1 = ff.create_distplot([x1,x2], [0,1], show_hist=False, colors=['green','red']).add_vline(x,line_width=2,line_dash="dash",annotation_text="Client",annotation_font_size=18)
+def distplots(data,var)
+
+plot_v1 = ff.create_distplot([x1,x2], [0,1], show_hist=False, colors=['green','red']).add_vline(x,line_width=2,line_dash="dash",line_color="orange",annotation_text="Client",annotation_font_size=18)
 plot_v1.update_layout(height=600)
 st.plotly_chart(plot_v1, use_container_width=True)
 
